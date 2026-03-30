4ee86051a4be1e3c66fe7c054a8e68f50e5ccb3f
Lost <saskefight@gmail.com>
2026-03-30 22:38:57 +0900

    Add files via upload

---
+++ Агрегация.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int id;
+public:
+    Student(int i, const string& n) : id(i), name(n) {
+        cout << "Студент '" << name << "' (ID:" << id << ") создан" << endl;
+    }
+
+    ~Student() {
+        cout << "Студент '" << name << "' (ID:" << id << ") удален" << endl;
+    }
+
+    void print() const {
+        cout << "  [ID:" << id << "] " << name << endl;
+    }
+
+    int getId() const { return id; }
+};
+
+class University {
+private:
+    string name;
+    vector<Student*> students;  
+public:
+    University(const string& n) : name(n) {
+        cout << "\nУниверситет '" << name << "' открыт" << endl;
+    }
+
+    ~University() {
+        cout << "Университет '" << name << "' закрыт" << endl;
+    }
+
+    void enrollStudent(Student* student) {
+        students.push_back(student); 
+    }
+
+    void printStudents() const {
+        cout << "\nСтуденты в " << name << ":" << endl;
+        for (const auto& student : students) {
+            student->print();
+        }
+    }
+
+    size_t getStudentCount() const {
+        return students.size();
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    
+    Student s1(1, "Иванов");
+    Student s2(2, "Петров");
+    Student s3(3, "Сидоров");
+
+    
+    University uni("МГУ");
+
+       
+    uni.enrollStudent(&s1);
+    uni.enrollStudent(&s2);
+    uni.enrollStudent(&s3);
+
+    uni.printStudents();
+
+    
+    
+
+    
+    s1.print();
+    s2.print();
+    s3.print();
+
+   
+
+    return 0;
+}
---
+++ Композиция.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Room {
+private:
+    string name;
+    double area;
+public:
+    Room(const string& n, double a) : name(n), area(a) {
+        cout << "Комната '" << name << "' создана" << endl;
+    }
+
+    ~Room() {
+        cout << "Комната '" << name << "' удалена" << endl;
+    }
+
+    void print() const {
+        cout << "  " << name << " (" << area << " м²)" << endl;
+    }
+};
+
+class House {
+private:
+    string address;
+    vector<Room> rooms; 
+public:
+    House(const string& addr) : address(addr) {
+        cout << "\nДом по адресу '" << address << "' построен" << endl;
+    }
+
+    ~House() {
+        cout << "Дом '" << address << "' снесен" << endl;
+    }
+
+    void addRoom(const string& name, double area) {
+        rooms.push_back(Room(name, area));  
+    }
+
+    void print() const {
+        cout << "\nДом: " << address << endl;
+        cout << "Комнаты:" << endl;
+        for (const auto& room : rooms) {
+            room.print();
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    
+    House house("ул. Ленина, 10");
+    house.addRoom("Гостиная", 25.5);
+    house.addRoom("Спальня", 15.0);
+    house.addRoom("Кухня", 12.0);
+
+    house.print();
+
+    
+ 
+    return 0;
+}
